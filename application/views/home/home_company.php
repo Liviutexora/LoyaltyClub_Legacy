b44bbe7680576a25abdbedab0b49f0fc116bce06
Constantin Uncuta <uncuta.constantin@gmail.com>
2022-03-16 08:16:09 +0200

    Compnay dashboard changes

--- application/views/home/home_company.php
+++ application/views/home/home_company.php
@@ -6,7 +6,7 @@
       <div class="card-body position-relative">
          <h6>Sales</h6>
          <div class="display-4 fs-4 mb-2 font-weight-normal text-sans-serif">
-            0.00 kr
+            <?=$amountValidatedTicket?> kr
          </div>
       </div>
    </div>
@@ -16,7 +16,7 @@
       <!--/.bg-holder-->
       <div class="card-body position-relative">
          <h6>Ticket</h6>
-         <div class="display-4 fs-4 mb-2 font-weight-normal text-sans-serif text-warning">0</div>
+         <div class="display-4 fs-4 mb-2 font-weight-normal text-sans-serif text-warning"><?=$nrOfTickets?></div>
       </div>
    </div>
    <div class="card mb-3 overflow-hidden" style="min-width: 12rem">
@@ -26,91 +26,21 @@
       <div class="card-body position-relative">
          <h6>Loyality Fee</h6>
          <div class="display-4 fs-4 mb-2 font-weight-normal text-sans-serif text-info" >0</div>
-       <a href="#" class="btn btn-success pt-1 pb-1 pl-2 pr-2 " style="font-size:14px; line-height:1;" data-toggle="modal" data-target="#general-modal1">Generate Invoice <i class="fa fa-angle-right"></i></a>
+       <!-- a href="#" class="btn btn-success pt-1 pb-1 pl-2 pr-2 " style="font-size:14px; line-height:1;" data-toggle="modal" data-target="#general-modal1">Generate Invoice <i class="fa fa-angle-right"></i></a -->
       </div>
    </div>
 </div>
 <div class="row no-gutters">
-   <button class="btn btn-primary w-100 mb-3">
-   Your Subscription will be renewed in
-   </button>
-   <div class="card mb-3 w-100">
-   <div class="card-header">
-      <div class="row align-items-center justify-content-between">
-         <div class="col-6 col-sm-auto  align-items-center pr-0">
-            <h5 class="fs-0 mb-2 text-nowrap py-2 py-xl-0"> Tickets</h5>
-           
-         </div>
-         <div class="col-6 col-sm-auto ml-auto text-right pl-0">
-         </div>
+      <button class="btn btn-primary w-100 mb-3">
+      <?=$this->lang->line("User Section Profile Label Profile Reference")?> <?=$this->current_user['id']?>
+      </button>
+      
+      <div class="w-100 mb-3"> 
+         <?php $this->load->view("company/tickets/partials/ticketsTable") ?>
       </div>
-   </div>
-   <div class="card-body  pt-0 card-datables">
-      <!-- <button class="btn btn-warning mr-1 mb-1 float-right " type="button" ajaxlink="<?=site_url('insert-ticket')?>" data-toggle="modal" data-target="#general-modal1">New Payment</button> -->
-      <button class="btn btn-warning mr-1 mb-1 float-right ">Add</button>
-      <button class="btn btn-warning mr-1 mb-1 float-right ">Download</button>
-      <div class="dashboard-data-table">
-         <table class="table table-sm table-dashboard fs--1 datatable-table tickets-table border-bottom" width="100%" data-language-label='{"Datatables Label Display" : "<?=$this->lang->line('Datatables Label Display')?>","Datatables Label Per Page" : "<?=$this->lang->line('Datatables Label Per Page')?>","Datatables Label No Data" : "<?=$this->lang->line('Datatables Label No Data')?>","Datatables Label Page" : "<?=$this->lang->line('Datatables Label Page')?>","Datatables Label Of" : "<?=$this->lang->line('Datatables Label Of')?>","Datatables Label No Records Available" : "<?=$this->lang->line('Datatables Label No Records Available')?>","Datatables Label Total Records" : "<?=$this->lang->line('Datatables Label Total Records')?>","Datatables Label Next" : "<?=$this->lang->line('Datatables Label Next')?>","Datatables Label Previous" : "<?=$this->lang->line('Datatables Label Previous')?>" }'>
-            <thead class="bg-200 text-900">
-               <tr>
-                <th class="sort pr-1 align-middle data-table-row-bulk-select"><?=$this->lang->line('Company Section Ticket Label Section Ticket Serial')?></th>
-                <th>Bonus</th>
-                <th class="sort pr-1 align-middle"><?=$this->lang->line('Company Section Ticket Label Section Ticket Value')?></th>
-                <th class="sort pr-1 align-middle"><?=$this->lang->line('Company Section Ticket Label Section Ticket Date')?></th>
-                <th>Client ID</th>
-                <th class="sort pr-1 align-middle"><?=$this->lang->line('Company Section Ticket Label Section Ticket Status')?></th>
-
-                  
-                <!--   <th class="sort pr-1 align-middle">Paid To </th>
-                  
-                  
-                  <th class="sort pr-1 align-middle data-table-row-bulk-select"><?=$this->lang->line('Company Section Ticket Label Section Ticket Serial')?></th> -->
-                  
-               </tr> 
-            </thead>
-            <tbody id="purchases">
-                 <tr>
-                   <td>WONFU</td>
-                   <td>0.00</td>
-                   <td>0.00</td>
-                   <td>02-030-2022</td>
-                   <td>0</td>
-                   <td><span class="badge badge-pill badge-soft-danger">No Used</span></td>
-               </tr>
-               <tr>
-                   <td>WONFU</td>
-                   <td>0.00</td>
-                   <td>0.00</td>
-                   <td>02-030-2022</td>
-                   <td>0</td>
-                   <td><span class="badge badge-pill badge-soft-danger">No Used</span></td>
-               </tr>
-               <tr>
-                   <td>WONFU</td>
-                   <td>0.00</td>
-                   <td>0.00</td>
-                   <td>02-030-2022</td>
-                   <td>0</td>
-                   <td><span class="badge badge-pill badge-soft-danger">No Used</span></td>
-               </tr>
-            </tbody>
-            <tfoot>
-                <tr>
-                    <th class="text-danger">Total</th>
-                    <th class="text-danger">0.00</th>
-                    <th class="text-danger">0.00</th>
-                    <th class="text-danger"></th>
-                    <th class="text-danger">3</th>
-                    <th class="text-danger"></th>
-                </tr>
-            </tfoot>
-         </table>
-      </div>
-   </div>
-</div>
 </div>
 <!-- Modal-->
-<div class="modal show" id="general-modal1" tabindex="-1" role="dialog" aria-labelledby="exampleModalLabel" aria-hidden="true" style="background-color: rgba(0,0,0,0.5)">
+<!-- div class="modal show" id="general-modal1" tabindex="-1" role="dialog" aria-labelledby="exampleModalLabel" aria-hidden="true" style="background-color: rgba(0,0,0,0.5)">
  <div class="modal-dialog" role="document">
    <div class="modal-content">
        <form action="<?=site_url('insert-ticket')?>" method="post" id="insert-ticket-form" class="needs-validation" novalidate>
@@ -130,4 +60,4 @@
        </form>
    </div>
  </div>
-</div>
+</div -->
--- application/views/layouts_after_login/index.php
+++ application/views/layouts_after_login/index.php
@@ -1,9 +1,13 @@
 <?php $this->load->view("layouts_after_login/header",array("allPagesDocumentation" =>(isset($this->allPagesDocumentation) ? $this->allPagesDocumentation : array()))) ?>
+  <?php
+  $jsFiles = [];
+  ?>
   <?php if(isset($this->current_user['tip']) && $this->current_user['tip'] == 1) { ?>
     <?php $this->load->view("home/home_user") ?>
   <?php } elseif(isset($this->current_user['tip']) && $this->current_user['tip'] == 2) { ?>
+    <?php $jsFiles[] = "company.js"; ?>
     <?php $this->load->view("home/home_company") ?>
   <?php } elseif(isset($this->current_user['tip']) && $this->current_user['tip'] == 3) { ?>
     <?php $this->load->view("home/home_admin") ?>
   <?php } ?>
-<?php $this->load->view("layouts_after_login/footer") ?>
+<?php $this->load->view("layouts_after_login/footer",array("jsFiles" => $jsFiles)) ?>
