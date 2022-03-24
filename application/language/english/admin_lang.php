f059f33e80bd928fb79d7ad94d2236cc7cd0dafc
Constantin Uncuta <uncuta.constantin@gmail.com>
2022-03-24 08:10:54 +0200

    Activate sending emails

--- application/language/english/admin_lang.php
+++ application/language/english/admin_lang.php
@@ -52,6 +52,8 @@ $lang['Admin Section Page Name Terms and Conditions'] = "Terms and Conditions";
 $lang['Admin Section Pages Page From Label Page Title'] = "Title";
 $lang['Admin Section Pages Page From Label Page Content'] = "Content";
 $lang['Admin Section Pages Page From Alert Page Content'] = "Please insert the page content";
+$lang['Admin Section Pages Login As Company Question'] = "Are you sure that you want login as this company?";
+
 
 
 
